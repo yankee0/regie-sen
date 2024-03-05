54890e7ad31ffd224d609c8e9674b1713c60879c
yankee <yankeesuprem@gmail.com>
2024-03-05 13:09:08 +0000

    Contact form regisen

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,6 +1,32 @@
+import { useFormik } from "formik";
 import React from "react";
+import emailjs from "@emailjs/browser";
 
 export default function ContactForm() {
+  const [sending, setSending] = React.useState(false);
+  const formik = useFormik({
+    initialValues: {},
+    onSubmit: (values, { resetForm }) => {
+      setSending(true);
+      emailjs
+        .send("service_hostinger_sdp", "template_8wodfg9", {
+          from_name: values.from_name,
+          message: values.message,
+          reply_to: values.reply_to,
+        },"cGQaPgsp-x-VJtFaw")
+        .then(() => {
+          setSending(false);
+          alert("Votre message a été envoyé avec succès !");
+          resetForm();
+        })
+        .catch(() => {
+          setSending(false);
+          alert(
+            "Une erreur s'est produite lors de l'envoi de votre message. Veuillez réessayer."
+          );
+        });
+    },
+  });
   return (
     <div id="contact" className="container pt-20 mx-auto p-3">
       <div className="grid gap-10 grid-flow-row md:grid-flow-col md:grid-cols-2">
@@ -67,7 +93,10 @@ export default function ContactForm() {
           </div>
         </div>
         <div>
-          <div className="card w-full bg-gradient-to-r from-secondary to-primary text-primary-content">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="card w-full bg-gradient-to-r from-secondary to-primary text-primary-content"
+          >
             <div className="card-body">
               <h2 className="card-title text-5xl font-bold text-base-100 mb-3">
                 Laissez nous un message
@@ -76,41 +105,42 @@ export default function ContactForm() {
                 Nous serions enchantés d'entrer en contact avec vous. Notre
                 équipe répondra à votre message dans les plus brefs délais.
               </p>
-              <form action="#">
+              <div>
                 <input
                   type="text"
                   placeholder="Votre nom complet"
                   className="input input-bordered w-full mb-3"
+                  onChange={formik.handleChange("from_name")}
+                  value={formik.values.from_name}
                   required
                 />
                 <input
                   type="email"
                   placeholder="Votre email"
                   className="input input-bordered w-full mb-3"
+                  onChange={formik.handleChange("reply_to")}
+                  value={formik.values.reply_to}
                   required
                 />
                 <textarea
+                  required
                   className="textarea textarea-bordered w-full"
                   placeholder="Votre message"
+                  onChange={formik.handleChange("message")}
+                  value={formik.values.message}
                 ></textarea>
-                <label className="cursor-pointer label gap-2">
-                  <input
-                    type="checkbox"
-                    className="toggle toggle-primary toget-xs"
-                  />
-                  <span className="label-text text-white text-opacity-80 mb-3">
-                    Recevoir de nos nouvelles et de nos conseils en s'abonnant à
-                    notre newsletter.
-                  </span>
-                </label>
-              </form>
+              </div>
               <div className="card-actions justify-end">
-                <button className="btn btn-primary text-base-100">
-                  Envoyer le message
+                <button
+                  disabled={sending}
+                  type="submit"
+                  className="btn btn-primary text-base-100"
+                >
+                  {sending ? "Envoi en cours..." : "Envoyer"}
                 </button>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
